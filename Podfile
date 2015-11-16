fa670862131217649cd846affd99cd04f413993a
Notboby <RobinFabre@t.home>
2015-11-16 11:05:57 +0100

    IOS SingleView app currency converter

---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+# use_frameworks!
+
+target 'peerReview3' do
+
+pod 'CurrencyRequest' , '~> 0.1'
+
+end
+
+target 'peerReview3Tests' do
+
+end
+
+target 'peerReview3UITests' do
+
+end
+
--- peerReview3/ViewController.m
+++ peerReview3/ViewController.m
@@ -7,21 +7,59 @@
 //
 
 #import "ViewController.h"
+#import "CurrencyRequest/CRCurrencyRequest.h"
+#import "CurrencyRequest/CRCurrencyResults.h"
 
-@interface ViewController ()
+
+@interface ViewController ()<CRCurrencyRequestDelegate>
+
+@property (nonatomic) CRCurrencyRequest *req;
+@property (weak, nonatomic) IBOutlet UITextField *inputField;
+@property (weak, nonatomic) IBOutlet UIButton *convertButton;
+@property (weak, nonatomic) IBOutlet UILabel *currencyA;
+@property (weak, nonatomic) IBOutlet UILabel *currencyB;
+@property (weak, nonatomic) IBOutlet UILabel *currencyC;
+
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (IBAction)buttonTapped:(id)sender {
+    self.convertButton.enabled = NO;
+    
+    self.req = [[CRCurrencyRequest alloc] init];
+    self.req.delegate = self;
+    [self.req start];
+    
+   
+    
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)currencyRequest:(CRCurrencyRequest *)req
+    retrievedCurrencies:(CRCurrencyResults *)currencies{
+    self.convertButton.enabled = YES;
+    
+    double inputValue = [self.inputField.text floatValue];
+    
+    double euroValue = inputValue * currencies.EUR;
+    double yenValue = inputValue * currencies.JPY;
+    double gbpValue = inputValue * currencies.GBP;
+    
+    NSString *eur = [NSString stringWithFormat:@"%.2f",euroValue];
+    NSString *yen = [NSString stringWithFormat:@"%.2f",yenValue];
+    NSString *gbp = [NSString stringWithFormat:@"%.2f",gbpValue];
+    
+    
+    
+    self.currencyA.text = eur;
+    self.currencyB.text = yen;
+    self.currencyC.text = gbp;
+
+    
+    
 }
+
 
 @end
